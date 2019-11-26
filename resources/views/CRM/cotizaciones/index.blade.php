090a2e06597799b85442a8be3af3ad55e369001f
MichaelADCR <56093588+MichaelADCR@users.noreply.github.com>
2019-11-26 16:31:03 -0600

    primero

--- resources/views/CRM/cotizaciones/index.blade.php
+++ resources/views/CRM/cotizaciones/index.blade.php
@@ -1,22 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4>Cotizaciones
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card col-md-3">
 
-                            <a href="{{ route('cotizaciones.create', $cotizaciones) }}" class="btn btn-sm btn-primary float-right">
-                                Crear
+                    <div class="panel-content">
+                        <h4>
+
+                            <a href="{{ route('cotizaciones.nueva', $cotizaciones) }}"
+                               class="btn btn-sm btn-primary float-right">
+                                Crear Nueva Cotizacion
                             </a>
 
-                    </h4>
+                        </h4>
+                    </div>
+
                 </div>
-                <div class="card-body table-responsive">
-                    <table class="table table-striped table-hover">
-                        <thead>
+
+                <div class="card">
+                    <div class="card-header">
+                        <div class="panel-content">
+                            <h4>Cotizaciones</h4>
+
+                        </div>
+
+                    </div>
+                    <div class="card-body table-responsive">
+                        <table class="table table-striped table-hover">
+                            <thead>
                             <tr>
                                 <th width="10px">ID</th>
                                 <th>Nombre</th>
@@ -25,47 +38,49 @@
 
                                 <th colspan="3">&nbsp;</th>
                             </tr>
-                        </thead>
-                        <tbody>
+                            </thead>
+                            <tbody>
                             @foreach ($cotizaciones as $cotizacion)
-                            <tr>
-                                <td>{{ $cotizacion->idEncabezadoCotizacion }}</td>
-                                <td>{{ $cotizacion->idCliente }}</td>
-                                <td>{{ $cotizacion->idEmpleado }}</td>
+                                <tr>
+                                    <td>{{ $cotizacion->idEncabezadoCotizacion }}</td>
+                                    <td>{{ $cotizacion->idCliente }}</td>
+                                    <td>{{ $cotizacion->idEmpleado }}</td>
 
 
-                                <td width="10px">
-                                    <a href="{{ route('cotizaciones.show', $cotizacion->idEncabezadoCotizacion) }}" class="btn btn-sm btn-success">
-                                        Ver
-                                    </a>
-                                </td>
-                                <td width="10px">
-                                    <a href="{{ route('cotizaciones.edit', $cotizacion->idEncabezadoCotizacion) }}" class="btn btn-sm btn-success">
-                                        Editar
-                                    </a>
+                                    <td width="10px">
+                                        <a href="{{ route('cotizaciones.show', $cotizacion->idEncabezadoCotizacion) }}"
+                                           class="btn btn-sm btn-success">
+                                            Ver
+                                        </a>
+                                    </td>
+                                    <td width="10px">
+                                        <a href="{{ route('cotizaciones.edit', $cotizacion->idEncabezadoCotizacion) }}"
+                                           class="btn btn-sm btn-success">
+                                            Editar
+                                        </a>
 
-                                </td>
-                                <td width="10px">
+                                    </td>
+                                    <td width="10px">
 
-                                    {!! Form::open(['route' => ['cotizaciones.destroy', $cotizacion->idEncabezadoCotizacion],
-                                    'method' => 'DELETE']) !!}
-                                    <button class="btn btn-sm btn-danger">Eliminar</button>
-                                    {!! Form::close() !!}
+                                        {!! Form::open(['route' => ['cotizaciones.destroy', $cotizacion->idEncabezadoCotizacion],
+                                        'method' => 'DELETE']) !!}
+                                        <button class="btn btn-sm btn-danger">Eliminar</button>
+                                        {!! Form::close() !!}
 
-                                </td>
-                            </tr>
+                                    </td>
+                                </tr>
                             @endforeach
-                        </tbody>
-                    </table>
-                    {{ $cotizaciones->links() }}
+                            </tbody>
+                        </table>
+                        {{ $cotizaciones->links() }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('script')
     <script>
-        document.getElementById('nav-sedes').className+=' active';
+        document.getElementById('nav-sedes').className += ' active';
     </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -18,38 +18,36 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
 
-//Route::resourse('clientes','CRM/ClienteController');
+Route::group(['middleware' => 'auth'], function () {
 
-//Pruebas
-//Route::get('prueba/sedes', 'SedeController@store')->name('sedes.store');
-// Route::get('prueba/cotizaciones/create', 'CotizacionController@create')->name('cotizaciones.create');
 //Sedes
-Route::get('sedes/index', 'SedeController@index')->name('sedes.index');
-Route::post('sedes/store', 'SedeController@store')->name('sedes.store');
-Route::get('sedes/create', 'SedeController@create')->name('sedes.create');
-Route::post('sedes/{sede}', 'SedeController@update')->name('sedes.update');
-Route::get('sedes/{sede}', 'SedeController@show')->name('sedes.show');
-Route::delete('sedes/{sede}', 'SedeController@destroy')->name('sedes.destroy');
-Route::get('sedes/{sede}/edit', 'SedeController@edit')->name('sedes.edit');
+    Route::get('sedes/index', 'SedeController@index')->name('sedes.index');
+    Route::post('sedes/store', 'SedeController@store')->name('sedes.store');
+    Route::get('sedes/create', 'SedeController@create')->name('sedes.create');
+    Route::post('sedes/{sede}', 'SedeController@update')->name('sedes.update');
+    Route::get('sedes/{sede}', 'SedeController@show')->name('sedes.show');
+    Route::delete('sedes/{sede}', 'SedeController@destroy')->name('sedes.destroy');
+    Route::get('sedes/{sede}/edit', 'SedeController@edit')->name('sedes.edit');
 
 //Cotizacion
-Route::get('cotizaciones/index', 'CotizacionController@index')->name('cotizaciones.index');
-Route::post('cotizaciones/store', 'CotizacionController@store')->name('cotizaciones.store');
-Route::get('cotizaciones/create', 'CotizacionController@create')->name('cotizaciones.create');
-Route::post('cotizaciones/{cotizacion}', 'CotizacionController@update')->name('cotizaciones.update');
-Route::get('cotizaciones/{cotizacion}', 'CotizacionController@show')->name('cotizaciones.show');
-Route::delete('cotizaciones/{cotizacion}', 'CotizacionController@destroy')->name('cotizaciones.destroy');
-Route::get('cotizaciones/{cotizacion}/edit', 'CotizacionController@edit')->name('cotizaciones.edit');
-Route::get('cotizaciones/nueva', 'CotizacionController@nueva')->name('cotizaciones.nueva');
+    Route::get('cotizaciones/index', 'CotizacionController@index')->name('cotizaciones.index');
+    Route::post('cotizaciones/store', 'CotizacionController@store')->name('cotizaciones.store');
+    Route::get('cotizaciones/create', 'CotizacionController@create')->name('cotizaciones.create');
+    Route::get('cotizaciones/nueva', 'CotizacionController@nueva')->name('cotizaciones.nueva');
+    Route::post('cotizaciones/{cotizacion}', 'CotizacionController@update')->name('cotizaciones.update');
+    Route::get('cotizaciones/{cotizacion}', 'CotizacionController@show')->name('cotizaciones.show');
+    Route::delete('cotizaciones/{cotizacion}', 'CotizacionController@destroy')->name('cotizaciones.destroy');
+    Route::get('cotizaciones/{cotizacion}/edit', 'CotizacionController@edit')->name('cotizaciones.edit');
+
 
 //Campana
-Route::get('campanas/index', 'CampanaController@index')->name('campanas.index');
-Route::post('campanas/store', 'CampanaController@store')->name('campanas.store');
-Route::get('campanas/create', 'CampanaController@create')->name('campanas.create');
-Route::put('campanas/{campana}', 'CampanaController@update')->name('campanas.update');
-Route::get('campanas/{campana}', 'CampanaController@show')->name('campanas.show');
-Route::delete('campanas/{campana}', 'CampanaController@destroy')->name('campanas.destroy');
-Route::get('campanas/{campana}/edit', 'CampanaController@edit')->name('campanas.edit');
+    Route::get('campanas/index', 'CampanaController@index')->name('campanas.index');
+    Route::post('campanas/store', 'CampanaController@store')->name('campanas.store');
+    Route::get('campanas/create', 'CampanaController@create')->name('campanas.create');
+    Route::put('campanas/{campana}', 'CampanaController@update')->name('campanas.update');
+    Route::get('campanas/{campana}', 'CampanaController@show')->name('campanas.show');
+    Route::delete('campanas/{campana}', 'CampanaController@destroy')->name('campanas.destroy');
+    Route::get('campanas/{campana}/edit', 'CampanaController@edit')->name('campanas.edit');
 
 
 //Detalle
@@ -64,23 +62,24 @@ Route::get('campanas/{campana}/edit', 'CampanaController@edit')->name('campanas.
 
 //vehiculo
 
-Route::get('vehiculos/index', 'VehiculoController@index')->name('vehiculos.index');
-Route::post('vehiculos/store', 'VehiculoController@store')->name('vehiculos.store');
-Route::get('vehiculos/create', 'VehiculoController@create')->name('vehiculos.create');
-Route::post('vehiculos/{vehiculo}', 'VehiculoController@update')->name('vehiculos.update');
-Route::get('vehiculos/{vehiculo}', 'VehiculoController@show')->name('vehiculos.show');
-Route::delete('vehiculos/{vehiculo}', 'VehiculoController@destroy')->name('vehiculos.destroy');
-Route::get('vehiculos/{vehiculo}/edit', 'VehiculoController@edit')->name('vehiculos.edit');
+    Route::get('vehiculos/index', 'VehiculoController@index')->name('vehiculos.index');
+    Route::post('vehiculos/store', 'VehiculoController@store')->name('vehiculos.store');
+    Route::get('vehiculos/create', 'VehiculoController@create')->name('vehiculos.create');
+    Route::post('vehiculos/{vehiculo}', 'VehiculoController@update')->name('vehiculos.update');
+    Route::get('vehiculos/{vehiculo}', 'VehiculoController@show')->name('vehiculos.show');
+    Route::delete('vehiculos/{vehiculo}', 'VehiculoController@destroy')->name('vehiculos.destroy');
+    Route::get('vehiculos/{vehiculo}/edit', 'VehiculoController@edit')->name('vehiculos.edit');
 
-//       Route::get('vehiculo', 'VehiculoController@index')->name('vehiculos.index');
-Route::get('cliente', 'ClienteController@index')->name('clientes.index');
-/*      Route::post('roles/store', 'Roles\RoleController@store')->name('roles.store')->middleware('can:roles.create');
-        Route::get('roles/create', 'Roles\RoleController@create')->name('roles.create')->middleware('can:roles.create');
-        Route::put('roles/{role}', 'Roles\RoleController@update')->name('roles.update')->middleware('can:roles.edit');
-        Route::get('roles/{role}', 'Roles\RoleController@show')->name('roles.show')->middleware('can:roles.show');
-        Route::delete('roles/{role}', 'Roles\RoleController@destroy')->name('roles.destroy')->middleware('can:roles.destroy');
-        Route::get('roles/{role}/edit', 'Roles\RoleController@edit')->name('roles.edit')->middleware('can:roles.edit');
-*/
+//Clientes
+    Route::get('cliente', 'ClienteController@index')->name('clientes.index');
+//    Route::post('vehiculos/store', 'VehiculoController@store')->name('vehiculos.store');
+//    Route::get('vehiculos/create', 'VehiculoController@create')->name('vehiculos.create');
+//    Route::post('vehiculos/{vehiculo}', 'VehiculoController@update')->name('vehiculos.update');
+//    Route::get('vehiculos/{vehiculo}', 'VehiculoController@show')->name('vehiculos.show');
+//    Route::delete('vehiculos/{vehiculo}', 'VehiculoController@destroy')->name('vehiculos.destroy');
+//    Route::get('vehiculos/{vehiculo}/edit', 'VehiculoController@edit')->name('vehiculos.edit');
+
+});
 
 Route::group(['middleware' => 'auth'], function () {
     Route::get('icons', ['as' => 'pages.icons', 'uses' => 'PageController@icons']);
@@ -100,4 +99,10 @@ Route::group(['middleware' => 'auth'], function () {
 
 }
 );
+
+Route::get('registro', 'Auth\RegisterController@registroMail')->name('Email.registroMail');
+//Route::get('registro', 'EmailController@registro')->name('Email.registro');
+
+
+
 
